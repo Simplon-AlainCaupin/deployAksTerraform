d708db8f4f4bbc490550f2f5daf478fce5d8a40b
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 10:24:57 +0100

    feat: added call to module in main.tf

--- main.tf
+++ main.tf
@@ -4,21 +4,9 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rgbriefterralain" {
-  name     = var.resource_group_name
-  location = "East US" # Replace with your desired region
-}
-
-resource "azurerm_virtual_network" "vnetbriefterralain" {
-  name                = var.vnet_name
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rgbriefterralain.location
-  resource_group_name = azurerm_resource_group.rgbriefterralain.name
-}
-
-resource "azurerm_subnet" "subnetbriefterralain" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rgbriefterralain.name
-  virtual_network_name = azurerm_virtual_network.vnetbriefterralain.name
-  address_prefixes     = ["10.0.1.0/24"]
+module "briefterra-lain" {
+  source                = "./module"
+  resource_group_name   = var.rgbriefterra-lain.name
+  vnet_name             = var.vnetbriefterra-lain.name
+  subnet_name           = var.subnetbriefterra-lain.name
 }
