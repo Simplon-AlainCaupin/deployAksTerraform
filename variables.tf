d4f37ce36a0ed8fcfd4390a17e6998348ffca00d
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 10:38:46 +0100

    fix: Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "network-config" {
   type         = object({
-    name           = string
-    location_name  = string
-    vnet_name      = string
-    subnet_name    = string
+    resource_group_name  = string
+    location_name        = string
+    vnet_name            = string
+    subnet_name          = string
     )}
     default      = {
-      name              = "briefterra-lain"
-      location_name     = "west europe"
-      vnet_name         = "briefterra-lain-vnet"
-      subnet_name       = "briefterra-lain-subnet"
+      resource_group_name  = "briefterra-lain"
+      location_name        = "west europe"
+      vnet_name            = "briefterra-lain-vnet"
+      subnet_name          = "briefterra-lain-subnet"
     }
 }
