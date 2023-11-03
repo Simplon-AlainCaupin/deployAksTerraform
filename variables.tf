86f616fddd13f50d6e05b128fe24780f3815fab0
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 09:54:56 +0100

    feat: Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,19 @@
 # Variables, à ne pas modifier
 
-variable "rg_name" {
-    description = "Nom du resource group"
-    type        = string
-    default     = "alain-rg-terra"
+variable "resource_group_name" {
+  description = "Nom du rg"
+  type        = string
+  default     = "briefterra-lain"
 }
 
-variable "rg_location"{
-    description = "region du rg"
-    type        = string
-    default     = "West Europe"
+variable "vnet_name" {
+  description = "Nom du vnet"
+  type        = string
+  default     = "briefterra-lain-vnet"
+}
+
+variable "subnet_name" {
+  description = "Nom du subnet"
+  type        = string
+  default     = "briefterra-lain-subnet"
 }
