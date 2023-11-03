b9d06d871e02ff246a1249cad030c407f4c14c16
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 10:06:30 +0100

    fix: changed variables.tf to be used by main.tf

--- module/variables.tf
+++ module/variables.tf
@@ -1,19 +1,24 @@
 # Variables, à ne pas modifier
 
 variable "resource_group_name" {
-  description = "Nom du rg"
-  type        = string
-  default     = "briefterra-lain"
+  description  = "Nom du rg"
+  type         = string
+  default      = "briefterra-lain"
 }
 
+variable "location" {
+  description  = "region du rg"
+  type         = string
+  default      = "West Europe"
+
 variable "vnet_name" {
-  description = "Nom du vnet"
-  type        = string
-  default     = "briefterra-lain-vnet"
+  description  = "Nom du vnet"
+  type         = string
+  default      = "briefterra-lain-vnet"
 }
 
 variable "subnet_name" {
-  description = "Nom du subnet"
-  type        = string
-  default     = "briefterra-lain-subnet"
+  description  = "Nom du subnet"
+  type         = string
+  default      = "briefterra-lain-subnet"
 }
