122275d3cf96221c6594235f0dbdcec84a7f4ab9
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 16:36:22 +0100

    fix: rewrote broken code

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
-# Fichier prinicpal pour la création du RG
-
 provider "azurerm" {
   features {}
 }
 
-module "briefterra-lain" {
-  source                = "./module"
-  resource_group_name   = var.rgbriefterra-lain.name
-  vnet_name             = var.vnetbriefterra-lain.name
-  subnet_name           = var.subnetbriefterra-lain.name
+module "brief13" {
+  source = "./module"  # Relative path to your module folder
+
+  resource_group_name     = var.resource_group_name
+  resource_group_location = var.resource_group_location
+  vnet_name               = var.vnet_name
+  vnet_address_space      = var.vnet_address_space
+  subnet_name             = var.subnet_name
+  subnet_address_prefix   = var.subnet_address_prefix
 }
