5418f5a5f23442aea9bfdf2d07b6652f6e48f5eb
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 09:53:11 +0100

    feat: Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = var.rg_name
-  location = var.rg_location
+resource "azurerm_resource_group" "rgbriefterralain" {
+  name     = var.resource_group_name
+  location = "East US" # Replace with your desired region
+}
+
+resource "azurerm_virtual_network" "vnetbriefterralain" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rgbriefterralain.location
+  resource_group_name = azurerm_resource_group.rgbriefterralain.name
+}
+
+resource "azurerm_subnet" "subnetbriefterralain" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rgbriefterralain.name
+  virtual_network_name = azurerm_virtual_network.vnetbriefterralain.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
