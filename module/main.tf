395025c8e6319f6cb5dada69d69849a117a6747f
Simplon-AlainCaupin <108000871+Simplon-AlainCaupin@users.noreply.github.com>
2023-11-03 10:09:16 +0100

    fix: Updated main.tf to use var file

--- module/main.tf
+++ module/main.tf
@@ -3,19 +3,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rgbriefterralain" {
-  name     = "briefterra-lain"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location_name
 }
 
 resource "azurerm_virtual_network" "vnetbriefterralain" {
-  name                = "briefterra-lain-vnet"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rgbriefterralain.location
   resource_group_name = azurerm_resource_group.rgbriefterralain.name
 }
 
 resource "azurerm_subnet" "subnetbriefterralain" {
-  name                 = "example-subnet"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rgbriefterralain.name
   virtual_network_name = azurerm_virtual_network.vnetbriefterralain.name
   address_prefixes     = ["10.0.1.0/24"]
